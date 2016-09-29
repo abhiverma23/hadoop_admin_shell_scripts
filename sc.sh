f96d0e436a9ce8b516aacb41b3bf1fad5a26461f
Abhishek Verma <abhishekverma3210@gmail.com>
2016-09-28 20:41:26 -0700

    sc.sh to parallel with master serial

--- sc.sh
+++ sc.sh
@@ -7,6 +7,8 @@
 clear
 CMD="sudo /sbin/shutdown -r 0"
 _T=-t
+_PT="-x '-tt'"
+_FLAG_=0
 ################## HELP ######################
 #if [ $1 -eq "--help" ]
 #then
@@ -14,13 +16,14 @@ _T=-t
 #	exit 24
 #fi
 ############# Node Selections ################
-NODE="slave slave1 slave2 slave3 slave4 master"
+NODE="slave slave1 slave2 slave3 slave4"
 echo	"Command(s): $CMD"
 echo -e "Target(s) :\c"
 if [ -z "$1" ]
 then 
 	echo " $NODE"
 else
+	_FLAG_=1
 	NODE=$*
 	echo " $NODE"
 fi
@@ -29,9 +32,22 @@ fi
 
 ############### COMMAND EXECUTION ##############
 
+pwd
 for n in $NODE
 do
-	echo -e "\n\n"$n" :"
-	ssh $n $_T $CMD
+	echo $n >> _TEMP_PSSH_USR_LST_
 done
 
+pssh -h _TEMP_PSSH_USR_LST_ $_PT -i "$CMD"
+if [ _FLAG_ -eq 0 ]
+then
+	ssh master $_T $CMD
+fi
+
+rm _TEMP_PSSH_USR_LST_
+
+#for n in $NODE
+#do
+#	echo -e "\n\n"$n" :"
+#	ssh $n $_T $CMD
+#done
